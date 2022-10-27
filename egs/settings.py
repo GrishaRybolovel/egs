9ee7bbe0a265b99925bbeb8cbe71f1cda269473b
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2022-10-27 22:48:43 +0300

    Primary model changes

--- egs/settings.py
+++ egs/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'egs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'egsDB',
+        'USER': 'grisharybolovlev',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
