bba9f7c502e3fb031c8da15136d913c04af4057e
hermandyudin <dyudin.german@mail.ru>
2022-11-02 02:59:41 +0300

    First request createsd

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
+     path('task/<int:id>/', get_task_by_id),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from core.models import *
+
+
+def get_task_by_id(request, id):
+    task = Tasks.objects.get(id=id)
+    info = {
+        'Название': task.name,
+        'Автор': task.author,
+        'Дата создания': task.created,
+        'Дата завершения': task.completion,
+    }
+    membersqs = Employees.objects.filter(employee_to_task__name=task.name)
+    members = []
+    for member in membersqs:
+        members.append(f'id:{member.id}, имя: {member.name}')
+    messagesqs = Messages.objects.filter(task__name=task.name)
+    messages=[]
+    for message in messagesqs:
+        messages.append(f'Автор: {message.author}, текст: {message.message}')
+    return HttpResponse(f'Информация: {info}\n Участники: {members}\n Сообщения: {messages}')
