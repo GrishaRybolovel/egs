a44602f23b2d96621bd52ea60f9e6d391ee4a678
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2023-01-26 01:25:43 +0300

    Authentication stuff

--- core/forms.py
+++ core/forms.py
@@ -4,6 +4,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control form-control-user'}), max_length=100)
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email',
+                                                          'class': 'form-control form-control-user'}), max_length=100)
+    password1 = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Password',
+                                                              'class': 'form-control form-control-user'}), max_length=100)
+    password2 = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Repeat Password',
+                                                              'class': 'form-control form-control-user'}), max_length=100)
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
