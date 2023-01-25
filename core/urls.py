4643e10d71d2978f505ebcbcfa5300b3cbece48b
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2023-01-25 21:13:35 +0300

    Some frontend stuff

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
      path('task/<int:id>/', get_task_by_id),
+     path('index.html', home),
+     path('login.html', login),
+     path('register.html', register),
+     path('forgot-password.html', forgot_password),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 from core.models import *
+
+from . models import Tasks
+
+
+def home(request):
+    tasks = Tasks.objects.all()
+    context = {
+        'tasks': tasks,
+        'user' : User.objects.all()
+    }
+    return render(request, 'core/index.html', context)
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        print(request.POST)
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login.html')
+    else:
+        form = CreateUserForm()
+    return render(request, 'core/register.html', {'form': form})
+
+def forgot_password(request):
+    return render(request, 'core/forgot-password.html')
 
 
 def get_task_by_id(request, id):
