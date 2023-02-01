12f378bf32b08165c908957253f3f883bfac35ae
Grisha Rybolovlev <rybolovlev.grisha@gmail.com>
2023-02-01 23:11:16 +0300

    Added divisions to admin panel

--- core/admin.py
+++ core/admin.py
@@ -29,8 +29,14 @@ class DocumentsAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'status', 'doc_type', 'duration', 'doc')
     search_fields = ('id', 'name')
 
+class DivisionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'parent_division')
+    list_display_links = ('id',)
+    search_fields = ('name',)
+
 admin.site.register(Employees, EmployeesAdmin)
 admin.site.register(Messages, MessagesAdmin)
 admin.site.register(Tasks, TasksAdmin)
 admin.site.register(Projects, ProjectsAdmin)
 admin.site.register(Documents, DocumentsAdmin)
+admin.site.register(Divisions, DivisionsAdmin)
