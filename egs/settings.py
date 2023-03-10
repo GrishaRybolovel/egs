21f558743ce525e9f4985ec1ae3379eddc3a12d6
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2023-03-10 16:42:50 +0300

    Update settings.py

--- egs/settings.py
+++ egs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2_d$5gsfv@0nc&u+$42n*q*rb2f&xr0wr2f4hz!iiex+sftfz9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
